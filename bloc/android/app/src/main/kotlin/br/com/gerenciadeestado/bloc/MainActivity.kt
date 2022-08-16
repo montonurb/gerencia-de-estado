be705580bb20c33900c3c2ef7c30eabf39346fba
montonurb <brunomoncar007@gmail.com>
2022-08-16 15:54:00 -0300

    adicionando pastas

---
+++ bloc/android/app/src/main/kotlin/br/com/gerenciadeestado/bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gerenciadeestado.bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ provider/android/app/src/main/kotlin/br/com/gerenciadeestado/provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gerenciadeestado.provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
